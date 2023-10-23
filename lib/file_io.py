ced5d218eda5f85c5664143e0d2342a2bdac2096
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-23 14:49:50 -0700

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as write_file:
+        write_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as append_file:
+        append_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="utf-8") as read_file:
+        for line in read_file:
+            return line
